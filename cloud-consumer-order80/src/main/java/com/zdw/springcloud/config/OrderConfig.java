fe01fb5692b04b01c08c5ad987c49e6fd3df5a4f
zengdongwen <623008719@qq.com>
2020-04-20 22:31:12 +0800

    eureka集群搭建

--- cloud-consumer-order80/src/main/java/com/zdw/springcloud/config/OrderConfig.java
+++ cloud-consumer-order80/src/main/java/com/zdw/springcloud/config/OrderConfig.java
@@ -1,5 +1,6 @@
 package com.zdw.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ public class OrderConfig {
      * @return
      */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- cloud-consumer-order80/src/main/java/com/zdw/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/zdw/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.zdw.springcloud.controller;
 import com.zdw.springcloud.entities.CommonResult;
 import com.zdw.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,8 @@ import javax.annotation.Resource;
 public class OrderController {
 
     //支付服务的地址
-    public static final String PAYMENT_URL = "http://localhost:8001";
+    //public static final String PAYMENT_URL = "http://localhost:8001";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource  //这是Java里面的注解
     RestTemplate restTemplate;
