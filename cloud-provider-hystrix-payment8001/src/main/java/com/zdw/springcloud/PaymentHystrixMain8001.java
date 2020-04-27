43624a9127b37ffb50f30cae5b08a68b231b5c41
zdwhong <34509115+zdwhong@users.noreply.github.com>
2020-04-27 14:52:10 +0800

    Update PaymentHystrixMain8001.java

--- cloud-provider-hystrix-payment8001/src/main/java/com/zdw/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/zdw/springcloud/PaymentHystrixMain8001.java
@@ -12,6 +12,18 @@ public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
     }
+    
+    
+    
+    
+    
+    
+    
+    //2222222222222222222
+    
+    
+    
+    //11111111111
 
     //这里会有其他的关于HystrixDashboard的Servlet需要注册
     //注册之后才能支持豪猪哥的监控
